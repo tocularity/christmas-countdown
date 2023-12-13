f6183752b30ef5bfa01c9e47da83e00ecc8fed77
tocularity <97557963+tocularity@users.noreply.github.com>
2023-12-13 01:43:35 +0000

    Fixes

--- components/Credit.jsx
+++ components/Credit.jsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 export default function Credit({ name, unsplash, image }) {
   return (
     <Link
-      href={`https://unplash.com/${unsplash}`}
+      href={`https://unsplash.com/${unsplash}`}
+      target="_blank"
       className="bg-none border-secondary hover:bg-secondary transition border-[1px] rounded px-2 py-2 flex flex-row items-center gap-2 w-full font-medium"
     >
       <Image src={image} width={32} height={32} className="rounded-full h-8 w-8 bg-neutral-300" alt={`${name}'s Avatar`} />
