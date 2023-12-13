684250ffd4373c30704ca4d4f824f025edd98bfb
tocularity <97557963+tocularity@users.noreply.github.com>
2023-12-13 01:18:12 +0000

    Create project

--- app/page.js
+++ app/page.js
@@ -1,113 +1,276 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { IoCheckmark, IoMenu, IoSave, IoSwapHorizontal } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa6";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import Credit from "@/components/Credit";
 
-export default function Home() {
+export default function Index() {
+  const [loadState, setLoadState] = useState(0);
+  const [days, setDays] = useState([]);
+  const [backgrounds] = useState([
+    {
+      img: "anniespratt", // Image at /bg/<>.jpg
+      text: "black",
+      name: "Annie Spratt",
+      unsplash: "@anniespratt",
+      image: "https://images.unsplash.com/profile-1648828806223-1852f704c58aimage",
+    },
+    {
+      img: "chadmadden",
+      text: "white",
+      name: "Chad Madden",
+      unsplash: "@chadmadden",
+      image: "https://images.unsplash.com/profile-1479493546391-c7a8f30d8f48",
+    },
+    {
+      img: "tessarampersad",
+      text: "white",
+      name: "Tessa Rampersad",
+      unsplash: "@t_rampersad",
+      image: "https://images.unsplash.com/profile-1493621054700-9f69ec529fbd",
+    },
+    {
+      img: "medpoole",
+      text: "black",
+      name: "Med Poole",
+      unsplash: "@medpoole",
+      image: "https://images.unsplash.com/profile-1580426429222-81d574542dbdimage",
+    },
+    {
+      img: "nathananderson",
+      text: "black",
+      name: "Nathan Anderson",
+      unsplash: "@nathananderson",
+      image: "https://images.unsplash.com/profile-1676320214445-e3fec6f7b87cimage",
+    },
+  ]);
+  const [currentBackground, setCurrentBackground] = useState("");
+  const [backgroundSaved, setBackgroundSaved] = useState(false);
+  const [colour, setColour] = useState("");
+
+  function pickBackground() {
+    const length = backgrounds.length;
+    const bgNumber = Math.floor(Math.random() * length);
+    setCurrentBackground(backgrounds[bgNumber].img);
+    setColour(backgrounds[bgNumber].text);
+  }
+
+  function switchBackground() {
+    setLoadState(3);
+    const length = backgrounds.length;
+    let bgNumber = Math.floor(Math.random() * length);
+    while (backgrounds[bgNumber].img === currentBackground) {
+      bgNumber = Math.floor(Math.random() * length);
+    }
+    setCurrentBackground(backgrounds[bgNumber].img);
+    setColour(backgrounds[bgNumber].text);
+    loadPage();
+  }
+
+  useEffect(() => {
+    pickBackground();
+  }, [backgrounds]);
+
+  useEffect(() => {
+    const savedBackground = localStorage.getItem("savedBackground");
+    setBackgroundSaved(currentBackground === localStorage.getItem("savedBackground"));
+    if (savedBackground && backgrounds.some((bg) => bg.img === savedBackground)) {
+      setBackgroundSaved(currentBackground === localStorage.getItem("savedBackground"));
+      setCurrentBackground(savedBackground);
+      setColour(backgrounds.find((bg) => bg.img === savedBackground).text);
+    }
+  }, [currentBackground]);
+
+  function saveBackground() {
+    localStorage.setItem("savedBackground", currentBackground);
+    setBackgroundSaved(currentBackground === localStorage.getItem("savedBackground"));
+  }
+
+  function unSaveBackground() {
+    localStorage.removeItem("savedBackground");
+    setBackgroundSaved(currentBackground === localStorage.getItem("savedBackground"));
+  }
+
+  useEffect(() => {
+    function timeUntilChristmas() {
+      let today = new Date();
+      let christmasYear = today.getFullYear();
+
+      if (today.getMonth() == 11 && today.getDate() > 25) {
+        christmasYear = christmasYear + 1;
+      }
+
+      let christmasDate = new Date(christmasYear, 11, 25);
+      let dayMilliseconds = 1000 * 60 * 60 * 24;
+      let daysUntil = Math.ceil((christmasDate.getTime() - today.getTime()) / dayMilliseconds);
+      setDays(daysUntil.toString().split(""));
+
+      setLoadState(3);
+    }
+    timeUntilChristmas();
+  }, [currentBackground]);
+
+  function loadPage() {
+    if (loadState === 3) {
+      setLoadState(4);
+      setInterval(() => {
+        setLoadState(5);
+      }, 500);
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <>
+      <main className="w-screen h-screen overflow-hidden">
+        <div
+          className={`block w-screen h-screen relative transition duration-1000 overflow-hidden ${
+            loadState === 5 ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+          }`}
+        >
+          <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center z-10 gap-5 font-serif">
+            <section className="flex flex-row gap-8 select-none">
+              {days.map((day) => (
+                <div
+                  key={day}
+                  className={`text-[10rem] ${
+                    colour === "white" ? "text-white" : "text-black"
+                  } font-extrabold bg-black bg-opacity-30 rounded-xl w-36 text-center shadow-2xl backdrop-blur`}
+                >
+                  {day}
+                </div>
+              ))}
+            </section>
+            <p className={`leading-4 text-xl font-semibold ${colour === "white" ? "text-white" : "text-black"}`}>Days 'til Christmas</p>
+          </div>
+          <section className="absolute top-0 left-0 w-full z-0 h-fit">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src={`/bg/${currentBackground}.jpg`}
+              width={1920}
+              height={1080}
+              className="w-full object-cover h-screen pointer-events-none select-none transition duration-300"
+              quality={100}
               priority
+              draggable={false}
+              onLoad={() => {
+                loadPage();
+              }}
+              alt=""
             />
-          </a>
+          </section>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <section
+          className={`absolute bottom-3 left-3 z-20 flex flex-row items-center gap-2 transition duration-500 ${
+            loadState === 5 ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+          }`}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <Dialog>
+            <DropdownMenu>
+              <DropdownMenuTrigger className={`${colour === "white" ? "text-white bg-black" : "text-black bg-white"} bg-opacity-50 p-1 rounded`}>
+                <IoMenu className="text-2xl" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-64 ml-3 mb-0.5">
+                <DropdownMenuLabel className="pb-0.5">Christmas Countdown</DropdownMenuLabel>
+                <DropdownMenuLabel className="text-neutral-300 text-xs pt-0 font-light">Time in UTC +0</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {!backgroundSaved ? (
+                  <button className="w-full" onClick={() => switchBackground()}>
+                    <DropdownMenuItem className="cursor-pointer pb-0 flex-col items-start">
+                      <div className="flex flex-row items-center gap-1">
+                        <IoSwapHorizontal />
+                        Switch Background
+                      </div>
+                      <p className="text-neutral-300 text-xs pt-0 pb-1.5 font-light">Get a new background</p>
+                    </DropdownMenuItem>
+                  </button>
+                ) : (
+                  <a title="You can't switch when you have a saved background.">
+                    <DropdownMenuItem className="cursor-pointer pb-0 flex-col items-start" disabled>
+                      <div className="flex flex-row items-center gap-1">
+                        <IoSwapHorizontal />
+                        Switch Background
+                      </div>
+                      <p className="text-neutral-300 text-xs pt-0 pb-1.5 font-light">Get a new background</p>
+                    </DropdownMenuItem>
+                  </a>
+                )}
+                {backgroundSaved ? (
+                  <button className="w-full" onClick={() => unSaveBackground()}>
+                    <DropdownMenuItem className={`cursor-pointer pb-0 flex-col items-start ${backgroundSaved ? "text-green-400" : ""}`}>
+                      <div className="flex flex-row items-center gap-1">
+                        <IoCheckmark />
+                        Background Saved
+                      </div>
+                      <p className="text-neutral-300 text-xs pt-0 pb-1.5 font-light">You'll always get this background</p>
+                    </DropdownMenuItem>
+                  </button>
+                ) : (
+                  <button className="w-full" onClick={() => saveBackground()}>
+                    <DropdownMenuItem className={`cursor-pointer pb-0 flex-col items-start ${backgroundSaved ? "text-green-400" : ""}`}>
+                      <div className="flex flex-row items-center gap-1">
+                        <IoSave />
+                        Save Background
+                      </div>
+                      <p className="text-neutral-300 text-xs pt-0 pb-1.5 font-light">Always get this background</p>
+                    </DropdownMenuItem>
+                  </button>
+                )}
+                <DialogTrigger asChild>
+                  <DropdownMenuItem className="cursor-pointer">Image Credits</DropdownMenuItem>
+                </DialogTrigger>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                <DropdownMenuSeparator />
+                <Link href="https://github.com/tocularity" target="_blank" className="w-full">
+                  <DropdownMenuItem className="cursor-pointer items-center gap-1.5">
+                    By Tocularity on
+                    <FaGithub />
+                  </DropdownMenuItem>
+                </Link>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DialogContent className="sm:max-w-md">
+              <DialogHeader>
+                <DialogTitle>Images from Unsplash</DialogTitle>
+                <div className="grid grid-cols-2 gap-3 pt-3">
+                  {backgrounds.map((credit) => (
+                    <Credit name={credit.name} unsplash={credit.unsplash} image={credit.image} />
+                  ))}
+                </div>
+              </DialogHeader>
+              <div className="flex items-center space-x-2">
+                <div className="grid flex-1 gap-2"></div>
+              </div>
+              <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                  <Button type="button" variant="secondary">
+                    Close
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className={`absolute top-0 left-0 w-full h-full flex items-center justify-center gap-5 transition duration-300 flex-col text-neutral-500 ${
+            loadState > 3 ? "opacity-0 pointer-events-none" : "opacity-100 pointer-events-auto"
+          }`}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <Image src="/snowglobe.gif" width={64} height={64} className="animate-smooth-spin w-16 h-auto" alt="Snow globe with snowflakes" />
+          Loading...
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ components/Credit.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Credit({ name, unsplash, image }) {
+  return (
+    <Link
+      href={`https://unplash.com/${unsplash}`}
+      className="bg-none border-secondary hover:bg-secondary transition border-[1px] rounded px-2 py-2 flex flex-row items-center gap-2 w-full font-medium"
+    >
+      <Image src={image} width={32} height={32} className="rounded-full h-8 w-8 bg-neutral-300" alt={`${name}'s Avatar`} />
+      <p className="whitespace-nowrap truncate">{name}</p>
+    </Link>
+  );
+}
